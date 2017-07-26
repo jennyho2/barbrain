e4ebbf4ed3b2d9d83a3c5d951079b3caf82c02f0
EL <el@admins-MacBook-Air.local>
2017-07-26 13:21:00 -0400

    basic DB connection

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,16 +9,25 @@ app.controller('mainController', function($scope, $localStorage, $sessionStorage
 		location.href = '#!staff';
 	};
 
-	$http({
-	    method: 'GET',
-	    url: 'https://api.omnivore.io/1.0/locations/ing4zrzT',
-	    headers: {'Api-Key': '017bab8e46fa4ca9bb92f54f427269a4'}
-	})
-	.then(function(data, status, headers, config) {
-	  console.log(data);
-	},function(data, status, headers, config) {
-	    console.log(data);
+	// $http({
+	//     method: 'GET',
+	//     url: 'https://api.omnivore.io/1.0/locations/ing4zrzT',
+	//     headers: {'Api-Key': '017bab8e46fa4ca9bb92f54f427269a4'}
+	// })
+	// .then(function(data, status, headers, config) {
+	//   console.log(data);
+	// },function(data, status, headers, config) {
+	//     console.log(data);
+	// });
+
+	$http.get("/goals")
+	.then(function(data,status,headers,config) {
+		$scope.dailyGoal = data.data[0].dailyGoal;
+		$scope.weeklyGoal = data.data[0].weeklyGoal;
+	},function(data, status, headers, config)  {
+		console.log('fail');
 	});
+
 });
 
 app.config(function($routeProvider) {
@@ -114,6 +123,14 @@ $( "#clickme" ).click(function() {
     return text === "Add" ? "Remove" : "Add";
     })
 });
-$(document).ready(function()  {
-	
-})
+
+// app.service("Contacts", function($http) {
+//   this.getContacts = function() {
+//     return $http.get("/contacts").
+//       then(function(response) {
+//         return response;
+//       }, function(response) {
+//         alert("Error retrieving contacts.");
+//       });
+//   }
+// });
